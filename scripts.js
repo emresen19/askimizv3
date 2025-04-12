333e9598237159a7d3270d7b92b82e6ca48d7f96
emresen19 <emreesen118@gmail.com>
2025-04-12 16:03:13 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,17 +1,6 @@
-window.onload = function() {
-    setTimeout(() => {
-        document.getElementById('loader').style.display = 'none';
-        document.querySelector('.container').style.display = 'flex';
-    }, 2000);
-    const audio = document.getElementById('bgMusic');
-    document.body.addEventListener('click', () => {
-        if (audio.paused) {
-            audio.play();
-        }
-    }, { once: true });
-};
-function startStory() {
-    if (confirm("Hazır mısın sevgilim?")) {
-        alert("O zaman başlayalım ❤️");
-    }
+function goToSection(id) {
+  const sections = document.querySelectorAll('.section');
+  sections.forEach(sec => sec.style.display = 'none');
+  const selected = document.getElementById(id);
+  if (selected) selected.style.display = 'block';
 }
